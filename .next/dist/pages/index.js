e8e8de852e9be8fc7571b79017ea0be999c90380
Ringaile Valiaugaite <ringaile@gmail.com>
2018-10-28 14:02:00 +0100

    getinitprops function

--- .next/dist/pages/index.js
+++ .next/dist/pages/index.js
@@ -4,20 +4,100 @@ Object.defineProperty(exports, "__esModule", {
 	value: true
 });
 
+var _regenerator = require('babel-runtime/regenerator');
+
+var _regenerator2 = _interopRequireDefault(_regenerator);
+
+var _asyncToGenerator2 = require('babel-runtime/helpers/asyncToGenerator');
+
+var _asyncToGenerator3 = _interopRequireDefault(_asyncToGenerator2);
+
+var _getPrototypeOf = require('babel-runtime/core-js/object/get-prototype-of');
+
+var _getPrototypeOf2 = _interopRequireDefault(_getPrototypeOf);
+
+var _classCallCheck2 = require('babel-runtime/helpers/classCallCheck');
+
+var _classCallCheck3 = _interopRequireDefault(_classCallCheck2);
+
+var _createClass2 = require('babel-runtime/helpers/createClass');
+
+var _createClass3 = _interopRequireDefault(_createClass2);
+
+var _possibleConstructorReturn2 = require('babel-runtime/helpers/possibleConstructorReturn');
+
+var _possibleConstructorReturn3 = _interopRequireDefault(_possibleConstructorReturn2);
+
+var _inherits2 = require('babel-runtime/helpers/inherits');
+
+var _inherits3 = _interopRequireDefault(_inherits2);
+
 var _react = require('react');
 
 var _react2 = _interopRequireDefault(_react);
+
+var _factory = require('../ethereum/factory');
+
+var _factory2 = _interopRequireDefault(_factory);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var _jsxFileName = '/Users/ringaile/Documents/etherworkspace/kickstart/pages/index.js?entry';
 
-exports.default = function () {
-	return _react2.default.createElement('h1', {
-		__source: {
-			fileName: _jsxFileName,
-			lineNumber: 4
+
+var CampaignIndex = function (_Component) {
+	(0, _inherits3.default)(CampaignIndex, _Component);
+
+	function CampaignIndex() {
+		(0, _classCallCheck3.default)(this, CampaignIndex);
+
+		return (0, _possibleConstructorReturn3.default)(this, (CampaignIndex.__proto__ || (0, _getPrototypeOf2.default)(CampaignIndex)).apply(this, arguments));
+	}
+
+	(0, _createClass3.default)(CampaignIndex, [{
+		key: 'render',
+		value: function render() {
+			return _react2.default.createElement('div', {
+				__source: {
+					fileName: _jsxFileName,
+					lineNumber: 13
+				}
+			}, this.props.campaigns[0]);
 		}
-	}, 'This is the campaign list page!!!');
-};
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbInBhZ2VzL2luZGV4LmpzIl0sIm5hbWVzIjpbIlJlYWN0Il0sIm1hcHBpbmdzIjoiOzs7Ozs7QUFBQSxBQUFPLEFBRVA7Ozs7Ozs7O2tCQUFlLFlBQU0sQUFDcEI7d0JBQU8sY0FBQTs7YUFBQTtlQUFBO0FBQUE7QUFBQSxFQUFBLEVBQVAsQUFBTyxBQUNQO0FBRkQiLCJmaWxlIjoiaW5kZXguanM/ZW50cnkiLCJzb3VyY2VSb290IjoiL1VzZXJzL3JpbmdhaWxlL0RvY3VtZW50cy9ldGhlcndvcmtzcGFjZS9raWNrc3RhcnQifQ==
+	}], [{
+		key: 'getInitialProps',
+		value: function () {
+			var _ref = (0, _asyncToGenerator3.default)( /*#__PURE__*/_regenerator2.default.mark(function _callee() {
+				var campaigns;
+				return _regenerator2.default.wrap(function _callee$(_context) {
+					while (1) {
+						switch (_context.prev = _context.next) {
+							case 0:
+								_context.next = 2;
+								return _factory2.default.methods.getDeployedCampaigns().call();
+
+							case 2:
+								campaigns = _context.sent;
+								return _context.abrupt('return', { campaigns: campaigns });
+
+							case 4:
+							case 'end':
+								return _context.stop();
+						}
+					}
+				}, _callee, this);
+			}));
+
+			function getInitialProps() {
+				return _ref.apply(this, arguments);
+			}
+
+			return getInitialProps;
+		}()
+	}]);
+
+	return CampaignIndex;
+}(_react.Component);
+
+exports.default = CampaignIndex;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbInBhZ2VzL2luZGV4LmpzIl0sIm5hbWVzIjpbIlJlYWN0IiwiQ29tcG9uZW50IiwiZmFjdG9yeSIsIkNhbXBhaWduSW5kZXgiLCJwcm9wcyIsImNhbXBhaWducyIsIm1ldGhvZHMiLCJnZXREZXBsb3llZENhbXBhaWducyIsImNhbGwiXSwibWFwcGluZ3MiOiI7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7QUFBQSxBQUFPLEFBQVE7Ozs7QUFDZixBQUFPLEFBQWE7Ozs7Ozs7OztJQUVkLEE7Ozs7Ozs7Ozs7OzJCQVFJLEFBQ1I7MEJBQU8sY0FBQTs7ZUFBQTtpQkFBQSxBQUFNO0FBQU47QUFBQSxJQUFBLE9BQU0sQUFBSyxNQUFMLEFBQVcsVUFBeEIsQUFBTyxBQUFNLEFBQXFCLEFBQ2xDOzs7Ozs7Ozs7Ozs7ZUFQd0Isa0JBQUEsQUFBUSxRQUFSLEFBQWdCLHVCQUFoQixBQUF1QyxBOztZQUF6RDtBO3lDQUVDLEVBQUMsV0FBRCxBOzs7Ozs7Ozs7Ozs7Ozs7Ozs7O0FBTG1CLEEsQUFhNUI7O2tCQUFBLEFBQWUiLCJmaWxlIjoiaW5kZXguanM/ZW50cnkiLCJzb3VyY2VSb290IjoiL1VzZXJzL3JpbmdhaWxlL0RvY3VtZW50cy9ldGhlcndvcmtzcGFjZS9raWNrc3RhcnQifQ==
--- ethereum/web3.js
+++ ethereum/web3.js
@@ -1,5 +1,16 @@
 import Web3 from 'web3';
 
-const web3 = new Web3(window.web3.currentProvider);
+let web3;
+
+if (typeof window !== 'undefined' && typeof window.web3 !== 'undefined'){
+	//we are in the browser and metamask is running
+	web3 = new Web3(window.web3.currentProvider);
+} else {
+	//we are on the server or user is not running metamask
+	const provider = new Web3.providers.HttpProvider(
+		'https://rinkeby.infura.io/v3/d70fe3c263c84f79b2f3ee592e1e77d0'
+	);
+	web3 = new Web3(provider);
+}
 
 export default web3;
